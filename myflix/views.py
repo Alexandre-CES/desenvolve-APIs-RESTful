bd83f6cffa023bc60b77a6abb1d2f61681e0c09f
Alexandre <alexandreces666@gmail.com>
2025-08-05 20:12:37 -0300

    rodando app

--- myflix/views.py
+++ myflix/views.py
@@ -1,3 +1,12 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+def users(request):
+    if request.method == 'GET':
+        user = {
+            'id': 1,
+            'name': 'Alexandre',
+        }
+        return JsonResponse(user, json_dumps_params={'ensure_ascii': False})
+    return

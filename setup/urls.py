48f66352fabd3b98e9a84a553d4bc9b10e05a4c4
Alexandre <alexandreces666@gmail.com>
2025-08-07 20:42:40 -0300

    real swagger and redoc

--- setup/urls.py
+++ setup/urls.py
@@ -16,17 +16,34 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from myflix.views import userViewSet, streamViewSet, listaViewSet, listaUser, listaStream
-from rest_framework import routers
+from rest_framework import routers, permissions
 
 router = routers.DefaultRouter()
 router.register('users', userViewSet, basename='users')
 router.register('streams', streamViewSet, basename='streams')
 router.register('listas', listaViewSet, basename='listas')
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title='API Documentação',
+        default_version='v1',
+        description='Documentação da API para o seu projeto Django',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email='seuemail@dominio.com'),
+        license=openapi.License(name='MIT License'),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny,],
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(router.urls)),
     path('users/<int:pk>/listas/',listaUser.as_view()),
     path('streams/<int:pk>/listas/',listaStream.as_view()),
+    path('swagger/',schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]

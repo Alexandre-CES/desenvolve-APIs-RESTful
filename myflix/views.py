affa3bb2adda53aa5e9cb78ee5be62c357d8dda9
Alexandre <alexandreces666@gmail.com>
2025-08-07 14:47:57 -0300

    viewsets

--- myflix/views.py
+++ myflix/views.py
@@ -1,12 +1,15 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework import viewsets
+from myflix.models import user, stream
+from myflix.serializers import userSerializer, streamSerializer
+
 
 # Create your views here.
-def users(request):
-    if request.method == 'GET':
-        user = {
-            'id': 1,
-            'name': 'Alexandre',
-        }
-        return JsonResponse(user, json_dumps_params={'ensure_ascii': False})
-    return
+class userViewSet(viewsets.ModelViewSet):
+    queryset = user.objects.all()
+    serializer_class = userSerializer
+
+class streamViewSet(viewsets.ModelViewSet):
+    queryset = stream.objects.all()
+    serializer_class = streamSerializer
